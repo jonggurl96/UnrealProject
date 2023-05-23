1b68d7f631990b3ec0556f1213ac3dbd0f2fbb1f
jonggurl96 <jonggurl96@naver.com>
2023-05-23 16:53:07 +0900

    Create Project

---
+++ Source/UnrealProject/UnrealProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALPROJECT_API AUnrealProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
